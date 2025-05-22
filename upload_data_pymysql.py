e613dc27bad611f4f8fc00bd86f8d265e88b2609
asd6139884 <asd6139884@gmail.com>
2025-05-22 11:34:14 +0800

    pymysql Upload Program

---
+++ upload_data_pymysql.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 22 11:08:16 2025
+
+@author: Chih-Yi
+"""
+import pandas as pd
+import configparser
+import pymysql
+
+if __name__ == "__main__":
+    file = 'data2.csv'
+    data = pd.read_csv(open(file))
+
+    config = configparser.ConfigParser()
+    config.read('./control/Setting.ini')
+
+    host = config['Set']['host']
+    username = config['Set']['user']
+    password = config['Set']['password']
+    database = config['Set']['database']
+    table_name = config['Set']['datatabel']
+
+    try:
+        connection = pymysql.connect(host=host,
+                                     user=username,
+                                     password=password,
+                                     database=database,
+                                     charset='utf8mb4',
+                                     cursorclass=pymysql.cursors.DictCursor)
+        print("Connected to MySQL database")
+
+        with connection.cursor() as cursor:
+            for _, row in data.iterrows():
+                sql_query = f"""
+                INSERT INTO {table_name} ({', '.join(row.keys())})
+                VALUES ({', '.join(['%s'] * len(row))})
+                ON DUPLICATE KEY UPDATE
+                {', '.join([f'{key}=VALUES({key})' for key in row.keys()])}
+                """
+                cursor.execute(sql_query, tuple(row))
+            connection.commit()
+            print(f"Data inserted/updated successfully")
+
+    except Exception as e:
+        print(f"Error: {e}")
+        connection.rollback()
+    finally:
+        connection.close()
+
